954110ae9f83db5a511f3ed89c014dfee9bc6663
OneZeroZeroOneOne <proxorenko200055@gmail.com>
2020-10-18 22:01:23 +0300

    !!

--- src/views/Aboutme.vue
+++ src/views/Aboutme.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Hello! I'm Volodymyr, i'm 19's guid from Kuiv. Since childhood I was interested in computer science. It all started with learning trivial language Pascal ​​in high school. As I got older, I became more immersed in computer science.</p>
+    <p>Hello!!! I'm Volodymyr, i'm 19's guid from Kuiv. Since childhood I was interested in computer science. It all started with learning trivial language Pascal ​​in high school. As I got older, I became more immersed in computer science.</p>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import SkillContainer from '../components/SkillContainer.vue';
+
 @Component({
   components: {
     SkillContainer,
